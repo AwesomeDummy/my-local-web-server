94413b65714f0b6b49a552714be9fcab8c8ff741
Pritabrata Mallik <pritabrata@codetantra.in>
2023-05-31 01:39:59 +0530

    built server

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.SERVER_PORT || 3000;
+
+// Add Access Control Allow Origin headers
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://yoursite.com");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    // "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.get("/api", (req, res) => {
+  res.json("Hello");
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
